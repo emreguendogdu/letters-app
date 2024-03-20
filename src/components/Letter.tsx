6b737d9a63bd2229d10d9aba0fb0f09560b40fcb
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-03-20 15:24:59 +0300

    style: Letter styles

--- src/components/Letter.tsx
+++ src/components/Letter.tsx
@@ -5,7 +5,7 @@ import { convertDate } from "@/utils/letters"
 export default function Letter({ letter }: any) {
   return (
     <article className="mb-4 mt-10 max-w-3xl min-h-96 container ml-auto mr-auto break-words">
-      <div className="mb-10 mt-2 flex gap-4 flex-col">
+      <div className="mb-6 mt-2 flex gap-2 flex-col">
         <div id="links"></div>
         <div className="mb-4 mt-2 font-light flex gap-2">
           <time>{convertDate(letter.createdAt)}</time>
@@ -13,12 +13,12 @@ export default function Letter({ letter }: any) {
           <span>Osman Gundogdu</span>
         </div>
         <div>
-          <h1 className="text-4xl inline-block font-extrabold capitalize after:border after:border-yellow-300 after:block">
+          <h1 className="text-4xl inline-block font-extrabold capitalize after:border after:border-cyan-800 after:block">
             {letter.title}
           </h1>
         </div>
       </div>
-      <Markdown className="markdown">{letter.letter}</Markdown>
+      <Markdown className="markdown text-xl">{letter.letter}</Markdown>
     </article>
   )
 }
